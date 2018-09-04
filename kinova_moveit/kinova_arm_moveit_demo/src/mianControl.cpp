ef1df47034bb1b4714f7405e9e9e5e478c62306d
Alvin <huiwen3304@gmail.com>
2018-09-04 16:18:48 +0800

    Add new test programs

---
+++ kinova_moveit/kinova_arm_moveit_demo/src/mianControl.cpp
@@ -0,0 +1,8 @@
+/*
+=========================JDX pick and place chanllenge===============
+==========================Kinova arm test============================
+This is the main program for handling picking and place task in JDX
+challenge. 
+*/
+
+// Head files
