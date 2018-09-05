ae8e59f0ff6b6949451298458fa50debd65b37de
Alvin <huiwen3304@gmail.com>
2018-09-05 11:46:29 +0800

    Add build_workScene class to handle obstacles in planning scene and a lot of clean ups

---
+++ kinova_moveit/kinova_arm_moveit_demo/include/add_scene_objects.h
@@ -0,0 +1,51 @@
+#ifndef ADD_SCENE_OBJECTS_H
+#define ADD_SCENE_OBJECTS_H
+
+#include "ros/callback_queue.h"
+#include "ros/ros.h"
+#include <geometric_shapes/shape_operations.h>
+#include <geometric_shapes/solid_primitive_dims.h>
+#include <geometry_msgs/PointStamped.h>
+#include <tf/tf.h>
+
+// moveit
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+// Std C++ headers
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class build_workScene {
+public:
+  build_workScene(ros::NodeHandle &nh);
+  void add_boxModel(const char *name, float length, float width, float height,
+                    geometry_msgs::Pose prePose);
+  void add_meshModel(const char *name, const char *path,
+                     geometry_msgs::Pose prePose);
+  void clear_WorkScene(const char *objName);
+
+private:
+  /* data */
+  ros::NodeHandle nh_;
+  ros::Publisher pub;
+  ros::Publisher pub_aco_;
+  ros::Publisher pub_planning_scene_diff_;
+
+  moveit_msgs::CollisionObject co_;
+  geometry_msgs::PoseStamped can_pose_;
+  // work scene
+  moveit_msgs::AttachedCollisionObject aco_;
+  moveit_msgs::PlanningScene planning_scene_msg_;
+};
+
+#endif // ADD_SCENE_OBJECTS_H
--- kinova_moveit/kinova_arm_moveit_demo/src/planWithObstacle.cpp
+++ kinova_moveit/kinova_arm_moveit_demo/src/planWithObstacle.cpp
@@ -4,10 +4,12 @@ Test moveit plan with obstacles in working space.
 For JDX task, the obstacles include: kinect in the rear of the arm,
 AGV mobile plantform beneath of the robot arm.
 */
-
 #include "id_data_msgs/ID_Data.h"
 #include "ros/callback_queue.h"
 #include "ros/ros.h"
+#include <add_scene_objects.h> // handle scene obstacles
+#include <geometric_shapes/shape_operations.h>
+#include <geometric_shapes/solid_primitive_dims.h>
 #include <geometry_msgs/PointStamped.h>
 #include <tf/tf.h>
 
@@ -28,36 +30,6 @@ AGV mobile plantform beneath of the robot arm.
 
 using namespace std;
 
-void addCollisionObjects(const moveit_msgs::CollisionObject *co_,
-                         const char *name, float length, float width,
-                         float height, geometry_msgs::Pose prePose) {
-
-  co_.header.frame_id = "root";
-  co_.header.stamp = ros::Time::now();
-
-  co_.id = name;
-  ROS_INFO_STREAM("Add collision object: " << co_.id);
-
-  co_.primitives.resize(1);
-  co_.primitives[0].type = shape_msgs::SolidPrimitive::BOX;
-  co_.primitives[0].dimensions.resize(geometric_shapes::SolidPrimitiveDimCount<
-                                      shape_msgs::SolidPrimitive::BOX>::value);
-  co_.operation = moveit_msgs::CollisionObject::ADD;
-
-  co_.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_X] = length;
-  co_.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Y] = width;
-  co_.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z] = height;
-
-  co_.primitive_poses.resize(1);
-  co_.primitive_poses[0].position.x = prePose.position.x;
-  co_.primitive_poses[0].position.y = prePose.position.y;
-  co_.primitive_poses[0].position.z = prePose.position.z;
-  co_.primitive_poses[0].orientation.x = prePose.orientation.x;
-  co_.primitive_poses[0].orientation.y = prePose.orientation.y;
-  co_.primitive_poses[0].orientation.z = prePose.orientation.z;
-  co_.primitive_poses[0].orientation.w = prePose.orientation.w;
-}
-
 int main(int argc, char **argv) {
   ros::init(argc, argv, "plan_with_obstacle");
 
@@ -74,8 +46,9 @@ int main(int argc, char **argv) {
   }
 
   //   define the target pose
+  // default validate pose: -0.2 -0.4 0.5  1.57 -1.57 0
   geometry_msgs::PoseStamped goal_pose;
-  goal_pose.header.frame_id = "world";
+  goal_pose.header.frame_id = "root";
   goal_pose.pose.position.x = atof(argv[1]);
   goal_pose.pose.position.y = atof(argv[2]);
   goal_pose.pose.position.z = atof(argv[3]);
@@ -95,30 +68,40 @@ int main(int argc, char **argv) {
   // the end-effector frame is declared in moveit config, can not be changed
   // group_arm.setEndEffectorLink("j2n6s300_link_6");
 
-  //   add collision objects to robot working space
-  ROS_INFO_STREAM("Add collision objects into robot working space");
-  std::vector<moveit_msgs::CollisionObject> collision_objects;
-  //   add kinect obstacles
-  moveit_msgs::CollisionObject kinect_obs;
-  geometry_msgs::Pose kinect_pose;
-  kinect_pose.position.x = 0.6;
-  kinect_pose.position.y = 0.0;
-  kinect_pose.position.z = 0.5;
-  tf::Quaternion q = tf::createQuaternionFromRPY(0, 1.57, 0); 
-  kinect_pose.orientation.x = q.x;
-  kinect_pose.orientation.y = q.y;
-  kinect_pose.orientation.z = q.z;
-  kinect_pose.orientation.w = q.w;
-  ROS_INFO_STREAM("Obstacle orientation:" << kinect_pose);
-  addCollisionObjects(kinect_obs, "kinect_obs", 1.0,
-                                                  0.8, 0.03, kinect_pose);
+  // add collision objects to robot working space
+  ros::NodeHandle n;
+  ROS_INFO("Insert scene objects in workspace");
+  build_workScene buildWorkScene(n);
+  geometry_msgs::Pose tablePose, kinectBasePose, kinectBodyPose;
 
-  //   push back all the collisition object into a vector container
-  collision_objects.push_back(kinect_obs);
+  tablePose.position.x = 0;
+  tablePose.position.y = 0;
+  tablePose.position.z = -0.03 / 2.0;
+  buildWorkScene.add_boxModel("table", 1.5, 1.5, 0.03, tablePose);
 
-  //   add objects to robot working space
-  moveit::planning_interface::PlanningSceneInterface plan_scene_interface;
-  plan_scene_interface.addCollisionObjects(collision_objects);
+  // add kinect stick obstacle
+  kinectBasePose.position.x = 0.4;
+  kinectBasePose.position.y = 0;
+  kinectBasePose.position.z = 0.3;
+  tf::Quaternion qkinect = tf::createQuaternionFromRPY(0, 1.57, 1.57);
+  //   ROS_INFO_STREAM("Kinect obs pose: " << qkinect);
+  kinectBasePose.orientation.x = qkinect[0];
+  kinectBasePose.orientation.y = qkinect[1];
+  kinectBasePose.orientation.z = qkinect[2];
+  kinectBasePose.orientation.w = qkinect[3];
+  buildWorkScene.add_boxModel("kinectBase", 0.6, 0.05, 0.02, kinectBasePose);
+
+  // add kinect body obstacle
+  kinectBodyPose.position.x = 0.40;
+  kinectBodyPose.position.y = -0.05;
+  kinectBodyPose.position.z = 0.65;
+  tf::Quaternion qbody = tf::createQuaternionFromRPY(0, 0, -1.57);
+  //   ROS_INFO_STREAM("Kinect obs pose: " << qbody);
+  kinectBodyPose.orientation.x = qbody[0];
+  kinectBodyPose.orientation.y = qbody[1];
+  kinectBodyPose.orientation.z = qbody[2];
+  kinectBodyPose.orientation.w = qbody[3];
+  buildWorkScene.add_boxModel("kinectBody", 0.1, 0.3, 0.1, kinectBodyPose);
 
   ROS_INFO("Collision setup finished");
 
